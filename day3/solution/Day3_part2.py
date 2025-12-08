91eef4aff5bd89e6796fc8001dc640f0ac9939a1
einebrotscheibe <leler.keck@gmail.com>
2025-12-08 23:11:48 +0100

    finished my solution for part2 of day3

---
+++ day3/solution/Day3_part2.py
@@ -0,0 +1,30 @@
+def readFileAndMakeList():
+    f = open(r"C:\ideas\AdventOfCode_2025\day3\input\inputData.txt")
+    return f.read().split("\n")
+
+def getFirstTopCharAndIndex (inputSequence):
+    top = 0
+    topCharIndex = 0
+
+    for index in range(0, len(inputSequence)):
+        char = inputSequence[index]
+        if int(char) > top:
+            top = int(char)
+            topCharIndex = index
+
+    return top, topCharIndex
+
+
+result = 0
+for numSequence in readFileAndMakeList():
+    print(numSequence)
+    tempIndex = -1
+    for i in range(12, 0, -1):
+        numToAdd, tempIndex2 = getFirstTopCharAndIndex(numSequence[tempIndex + 1 : len(numSequence) - i + 1])
+        tempIndex += tempIndex2 + 1
+        print("tempIndex is: " + str(tempIndex))
+        temp = numToAdd * (10**(i-1))
+        print(temp)
+        result += temp
+
+print(result)
