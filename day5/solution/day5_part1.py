2a39ed8a6ee59ee39049e84393e7e6153d29300a
einebrotscheibe <leler.keck@gmail.com>
2025-12-21 21:44:03 +0100

    finished day 5 part1

--- day5/solution/day5_part1.py
+++ day5/solution/day5_part1.py
@@ -5,3 +5,26 @@ def readFileAndReturnLists():
     currentDir = Path(__file__).parent
     filePath = currentDir.parent / "input" / "inputData.txt"
     f = open(filePath, "r")
+    parts = f.read().split("\n\n")
+    loadedRanges = parts[0].split("\n")
+    finalRanges = []
+    for i in loadedRanges:
+        finalRanges.append(i.split("-"))
+    loadedNums = parts[1].split("\n")
+    return finalRanges, loadedNums
+
+ranges, nums = readFileAndReturnLists()
+print(ranges)
+print(nums)
+
+result = 0
+for numString in nums:
+    number = int(numString)
+    for rangeStrings in ranges:
+        begin = int(rangeStrings[0])
+        end = int(rangeStrings[1]) + 1
+        if number in range(begin, end):
+            result += 1
+            break
+
+print(result)
