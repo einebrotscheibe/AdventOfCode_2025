0024e1beecc758489d7b419a82804230db5fa78a
einebrotscheibe <leler.keck@gmail.com>
2025-12-25 00:57:02 +0100

    finally finished day6 part2 YAYYY

--- day6/solution/day6_part2.py
+++ day6/solution/day6_part2.py
@@ -5,36 +5,55 @@ def readFileAndReturnList():
     currentDir = Path(__file__).parent
     filePath = currentDir.parent / "input" / "inputData.txt"
     f = open(filePath, "r")
-    parts = f.read().split("\n\n")
-    loadedRanges = parts[0].split("\n")
-    finalRanges = []
-    for singularRange in loadedRanges:
-        finalRanges.append(singularRange.split("-"))
-    return finalRanges
+    data = f.read().split("\n")
+    numberLines = data[:-1]
 
-ranges = readFileAndReturnList()
-print(ranges)
+    tempOperations = data[-1].split(" ")
+    operations = []
+    for operation in tempOperations:
+        if operation.strip() != "":
+            operations.append(operation)
 
-result = 0
-rangeInts = []
-for rangeStrings in ranges:
-    begin = int(rangeStrings[0])
-    end = int(rangeStrings[1]) + 1
-    rangeInts.append([begin, end])
+    return numberLines, operations
 
-rangeInts.sort(key= lambda x: x[0])
+def returnSolution():
+    toReturn = 0
+    tempReturn = 0
+    numberLines, operations = readFileAndReturnList()
+    operationCounter = 0
+    for index in range(0, len(numberLines[0])):
+        operation = operations[operationCounter]
+        if index == 0 and operation == "*":
+            tempReturn = 1
+        if isBlankBetweenTwoNumbers(index, numberLines):
+            toReturn += tempReturn
+            operationCounter += 1
+            if operations[operationCounter] == "+":
+                tempReturn = 0
+            else:
+                tempReturn = 1
+            continue
 
-addedRanges = []
-for singleRange in rangeInts:
-    start = singleRange[0]
-    finish = singleRange[1]
-    for toTestRange in addedRanges:
-        if start in range(toTestRange[0], toTestRange[1]):
-            start = toTestRange[1]
-    if start < finish:
-        addedRanges.append([start, finish])
-        print(addedRanges)
+        numberToChangeBy = ""
+        for numberLine in numberLines:
+            if numberLine[index] == " ":
+                numberToChangeBy = numberToChangeBy + "y"
+            else:
+                numberToChangeBy = numberToChangeBy + str(numberLine[index])
+        numberToChangeBy = numberToChangeBy.lstrip("y").rstrip("y").replace("y", "0")
 
-for i in addedRanges:
-    result += i[1] - i[0]
-print(result)
+        if operation == "+":
+            tempReturn += int(numberToChangeBy)
+        else:
+            tempReturn *= int(numberToChangeBy)
+
+    toReturn += tempReturn
+    return toReturn
+
+def isBlankBetweenTwoNumbers(index, numberLines):
+    for numberLine in numberLines:
+        if numberLine[index] != " ":
+            return False
+    return True
+
+print(returnSolution())
